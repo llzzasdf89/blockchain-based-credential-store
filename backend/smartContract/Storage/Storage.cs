9be5efeeee9220262a7e7a851f42ec6194e1d6dc
RichardZhiLi <948945081@qq.com>
2022-08-02 21:35:16 +0100

    Write Storage contract, for storing file hashs from IPFS.

--- backend/smartContract/Storage/Storage.cs
+++ backend/smartContract/Storage/Storage.cs
@@ -1,9 +1,9 @@
+﻿using System;
 using Neo;
-using Neo.SmartContract;
 using Neo.SmartContract.Framework;
-using Neo.SmartContract.Framework.Native;
+using Neo.SmartContract.Framework.Attributes;
 using Neo.SmartContract.Framework.Services;
-using System;
+using Neo.SmartContract.Framework.Native;
 
 namespace Storage
 {
@@ -14,36 +14,32 @@ namespace Storage
     [ContractPermission("*", "onNEP17Payment")]
     public class Storage : SmartContract
     {
-        //TODO: Replace it with your own address.
-        [InitialValue("NiNmXL8FjEUEs1nfX9uHFBNaenxDHJtmuB", ContractParameterType.Hash160)]
-        static readonly UInt160 Owner = default; //default value is the developer of contract's address
-        StorageMap fileHashs = new(Storage.CurrentContext, nameof(fileHashs));
+         static readonly UInt160 Owner = default; //default value is the developer of contract's address
+        private static StorageMap fileHashs = new StorageMap(Neo.SmartContract.Framework.Services.Storage.CurrentContext,(ByteString)"fileHashs");
         private static bool IsOwner() => Runtime.CheckWitness(Owner);
 
         public static Boolean setfileHash (String address, String filehash){
-            byte[] res = this.fileHashs.Get(address);
-            if(res.Length == 0 || res == null) {
-                // if this array does not exist, create it and store the hash value inside it.
-                res = new byte[1];
+            string[] res = (string[])StdLib.Deserialize(fileHashs.Get(address))??new string[0];
+            if(res.Length == 0) {
+                res = new string[1];
                 res[0] = filehash;
-                this.fileHashs.Put(address,res);
+                fileHashs.Put(address,StdLib.Serialize(res));
                 return true;
-                }
-            byte[] tmp = new byte[res.Length + 1];
-            res.CopyTo(tmp,0);
+            }
+            string[] tmp = new string[res.Length+1];
+            //copy the array;Note in the Smart Contract, it does not support array.clone or array.copy. We could only copy an array in this way
+            for(int i = 0 ; i < res.Length; i++) tmp[i] = res[i]; 
             tmp[tmp.Length - 1] = filehash;
-            this.fileHashs.Put(address,res);
+            fileHashs.Put(address,StdLib.Serialize(tmp));
             return true;
         }
-        public static byte[] getfileHash(String address){
-            byte[] res = this.fileHashs.Get(address)?new byte[0]:this.fileHashs.Get(address);
+        public static string[] getfileHash(String address){
+            string[] res = (string[])StdLib.Deserialize(fileHashs.Get(address))??new string[0];
             return res;
         }
         // When this contract address is included in the transaction signature,
         // this method will be triggered as a VerificationTrigger to verify that the signature is correct.
         // For example, this method needs to be called when withdrawing token from the contract.
         public static bool Verify() => IsOwner();
-
-        
     }
 }
